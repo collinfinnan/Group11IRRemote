6c46a6a32c551d657872072c2a76ca432192f1be
Collin Finnan <cfinnan@nd.edu>
2023-02-21 10:14:03 -0500

    Clear

--- README.md
+++
@@ -1,4 +0,0 @@
-# Group11IRRemote
-Senior Design '23
-Group 11
-Infrared Remote Control Project
--- src/Website/website.h
+++
@@ -1,6 +0,0 @@
-#include <WiFi.h>
-
-#define NETWORK_NAME "SDNet"
-#define NETWORK_PW "CapstoneProject"
-
-bool start_WiFi(); 
